5b7be5b8b10717ff3b75602f63fdb39eb0353d05
djaydev <33335484+djaydev@users.noreply.github.com>
2020-12-04 10:52:42 -0500

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -3,10 +3,12 @@ export HOME=/config
 if [ "$THEME" = "light" ]; then
 mkdir -p /config/xdg/config/gtk-3.0
 printf "[Settings]\ngtk-application-prefer-dark-theme=0" > /config/xdg/config/gtk-3.0/settings.ini
+aria2c --enable-rpc=true -D --disable-ipv6 --check-certificate=false
 uget-gtk
 fi
 if [ "$THEME" = "dark" ]; then
 mkdir -p /config/xdg/config/gtk-3.0
 printf "[Settings]\ngtk-application-prefer-dark-theme=1" > /config/xdg/config/gtk-3.0/settings.ini
+aria2c --enable-rpc=true -D --disable-ipv6 --check-certificate=false
 uget-gtk
 fi
